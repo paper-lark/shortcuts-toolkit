322ab7761cbed894b2190e6cca7b5afdadba151a
Max Zhuravsky <paperlark@yandex.com>
2022-12-07 19:51:58 +0300

    Initial Commit

---
+++ shortcuts-toolkitUITests/shortcuts_toolkitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  shortcuts_toolkitUITestsLaunchTests.swift
+//  shortcuts-toolkitUITests
+//
+//  Created by Max Zhuravsky on 07.12.2022.
+//
+
+import XCTest
+
+final class shortcuts_toolkitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
